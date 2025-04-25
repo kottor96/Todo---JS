4ab1719bbeccb360af9e7ae282e4b9e84c1da1b6
kottor <gardien243@gmail.com>
2025-04-25 11:31:54 +0200

    j'ai mis un header et un main avec un titre dans le header

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,10 @@
 let body = document.getElementsByTagName('body')[0];
 let header = document.createElement('header');
 let main = document.createElement('main');
+let h1 = document.createElement('h1')
+let h1Content = document.createTextNode('To Do List')
+
+body.appendChild(header)
+body.appendChild(main)
+header.appendChild(h1)
+h1.appendChild(h1Content)
