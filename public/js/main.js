29c5fffcc58290983f680f7187f5b123048be4ff
kottor <gardien243@gmail.com>
2025-04-25 11:46:00 +0200

    input + button div1

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,26 @@
+// get element
 let body = document.getElementsByTagName('body')[0];
+
+// create
 let header = document.createElement('header');
 let main = document.createElement('main');
+// h1
 let h1 = document.createElement('h1')
+// div form
+let divForm = document.createElement('div')
+// input
+let input = document.createElement('input')
+// button
+let buttonAdd = document.createElement('button')
+let buttonAll = document.createElement('button')
+let buttonFini = document.createElement('button')
+let buttonPasfini = document.createElement('button')
+
+// create content
 let h1Content = document.createTextNode('To Do List')
 
+// append
 body.appendChild(header)
-body.appendChild(main)
+body.appendChild(main).appendChild(input,buttonAdd)
 header.appendChild(h1)
 h1.appendChild(h1Content)
